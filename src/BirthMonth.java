529a1a14312fad419d29a962cc8b73dc513d892c
velasqkd <velasqkd@mail.uc.edu>
2025-02-20 10:41:22 -0500

    Update BirthMonth.java

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -1,9 +1,26 @@
 import java.util.Scanner;
-public class Main {
+public class BirthMonth {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int BirthMonth = 0
-                int birthMonth
 
+        System.out.println("Please enter your birth month [1-12]: ");
+        if (in.hasNextInt())
+        {
+            int birthMonth = in.nextInt();
+            in.nextLine();
+            if (birthMonth >= 1 && birthMonth <= 12)
+            {
+                System.out.println("Your birth month is: " + birthMonth);
+            }
+            else
+            {
+                System.out.println("You entered an incorrect month value: " + birthMonth);
+            }
+        }
+        else
+        {
+            String trash = in.nextLine();
+            System.out.println("You entered an incorrect month value: " + trash);
+        }
     }
 }
